3b5d90bf156eaded1e745e6c759d0af3a4ead7ad
Kevin Gatamu <kevingatamumuthoni@gmail.com>
2024-09-16 16:29:47 +0300

    director interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,3 +1,6 @@
+/**
+ * Declare Teacher Interface
+ */
 interface Teacher {
   readonly firstName: string,
   readonly lastName: string,
@@ -6,3 +9,26 @@ interface Teacher {
   yearsOfExperience?: number;
   [propName: string]: any
 }
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);
+
+interface Directors extends Teacher{
+  numberOfReports: number
+}
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+console.log(director1);
